22d60f4f3826f80de157297287e90e67f28a0e87
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-09-24 15:04:27 +0800

    feat: create new component Pagination declare its typeSafe Props and events

---
+++ src/components/Pagination.tsx
@@ -0,0 +1,31 @@
+type PaginationProps = {
+  totalPages: number;
+  itemsPerPage: number;
+  currentPage: number;
+  onChangePage: (page: number) => void;
+};
+
+const Pagination = ({
+  totalPages,
+  itemsPerPage,
+  currentPage,
+  onChangePage,
+}: PaginationProps) => {
+  const totalItems = Math.ceil(totalPages / itemsPerPage);
+
+  return (
+    <div>
+      {Array.from({ length: totalItems }, (_, index) => (
+        <button
+          key={index}
+          disabled={index + 1 === currentPage}
+          onClick={() => onChangePage(index + 1)}
+        >
+          {index + 1}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Pagination;
