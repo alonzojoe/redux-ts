84a2a7d4b5f98c97a83fb3a0d11379a7b77b3e4f
Joenell Alonzo <72643848+alonzojoe@users.noreply.github.com>
2024-09-24 15:56:32 +0800

    refactor: modify products thunks add new params limit and offset to be used in pagination

--- src/store/thunks/posts.tsx
+++ src/store/thunks/posts.tsx
@@ -2,10 +2,13 @@ import { createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import { Products } from "../../libs/types";
 
-export const fetchProducts = createAsyncThunk("api/v1/products", async () => {
-  const response = await axios<Products[]>(
-    `https://api.escuelajs.co/api/v1/productssss`
-  );
+export const fetchProducts = createAsyncThunk(
+  "api/v1/products",
+  async ({ offset, limit }: { offset: number; limit: number }) => {
+    const response = await axios<Products[]>(
+      `https://api.escuelajs.co/api/v1/products?offset=${offset}&limit=${limit}`
+    );
 
-  return response.data;
-});
+    return response.data;
+  }
+);
